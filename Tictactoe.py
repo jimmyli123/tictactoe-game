ead81f9e4e3544eac4ecaec93528d8fbac1565a2
Jimmy Li <lijjimmy@gmail.com>
2019-10-06 16:48:23 -0400

    Adding function to figure out players

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,18 +1,24 @@
-def gameboard():
+
+# This is going to be a tic tac toe game
+
+
+
+winner = False
+
+# Playerdata is our function to determine which player is X and which is O
+def playerdata():
+	player1 = input("Would you like to be X or O: ").upper()
+	player2 = ''
+	if player1 == "X":
+		player2 = "O"
+	else: player2 ="X"
+	print(f"Player 1 is {player1}.\nPlayer 2 is {player2}")
+
+	player_list = [player1, player2]
+	return player_list
+
+players = playerdata()
+print(players)
+
+def playgame():
 	pass
-
-
-def display():
-	line = '|'
-	empty = ' '
-	gameboard1 = f"{empty} {line} {empty} {line} {empty}"
-	gameboard2 = f"{empty} {line} {empty} {line} {empty}"
-	gameboard3 = f"{empty} {line} {empty} {line} {empty}"
-	
-							
-	print(gameboard1)
-	print(gameboard2)
-	print(gameboard3)
-	
-display()
-	

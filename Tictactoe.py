80d311092281c0d67737564930f1c8fa4b23c610
Jimmy Li <lijjimmy@gmail.com>
2019-10-06 00:04:24 -0400

    My First File

---
+++ Tictactoe.py
@@ -0,0 +1,18 @@
+def gameboard():
+	pass
+
+
+def display():
+	line = '|'
+	empty = ' '
+	gameboard1 = f"{empty} {line} {empty} {line} {empty}"
+	gameboard2 = f"{empty} {line} {empty} {line} {empty}"
+	gameboard3 = f"{empty} {line} {empty} {line} {empty}"
+	
+							
+	print(gameboard1)
+	print(gameboard2)
+	print(gameboard3)
+	
+display()
+	
